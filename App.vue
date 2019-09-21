1fe0f92506d701667c2c4e49d82d0321f0c7ac3a
Lawrence Vanderpool <lawrence@literatibooks.com>
2019-09-21 12:21:31 -0500

    simple init

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Testing {{ foo }}</p>
+</template>
+
+<script>
+export default {
+  data () {
+    foo: 'bar'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  color: red;
+}
+</style>
